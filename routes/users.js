18a7bb0664618f83702246bf87a6a7249add74a2
Aziz Sebri <sebriaziz2016@gmail.com>
2024-04-23 10:16:05 +0000

    Remove unused dependencies and files

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,6 @@ import authenticateUser from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", authenticateUser, authController.loginUser);
+router.post("/",  authController.loginUser);
 
 export default router;
