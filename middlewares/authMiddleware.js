fc8ee0388f713b27cebd900e44b6be98b96a6f93
Aziz Sebri <sebriaziz2016@gmail.com>
2024-04-25 01:16:23 +0000

    Refactor user authentication code to use argon2 password hashing library and update login and register routes

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -6,10 +6,9 @@ async function authenticateUser(req, res, next) {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(401).send("Invalid email or password");
+      return res.status(401).send("Invalid email or password !!!");
     }
-    const hachedPassword = await argon2.hash(password);
-    const passwordMatch = await argon2.verify(user.password, hachedPassword);
+    const passwordMatch = await argon2.verify(user.password, password);
     if (!passwordMatch) {
       return res.status(401).send("Invalid email or password");
     }
--- routes/login.js
+++ routes/login.js
@@ -2,9 +2,8 @@ import express from "express";
 import authController from "../controllers/authController.js";
 import authenticateUser from "../middlewares/authMiddleware.js";
 
-
 const router = express.Router();
 
-router.post("/", authController.loginUser);
+router.post("/", authenticateUser, authController.loginUser);
 
 export default router;
