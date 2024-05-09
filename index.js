65ffdbb2336a92dd408dc4f395b84c1642a7b69b
Aziz Sebri <sebriaziz2016@gmail.com>
2024-05-09 16:53:46 +0000

    chore: Update CORS middleware to allow specific origin

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import cors from "cors";
 import * as dotenv from "dotenv";
 import connectDB from "./config/connection.js";
 import registerRoute from "./routes/register.js";
@@ -12,13 +11,20 @@ import ordersRoute from "./routes/orders.js";
 dotenv.config();
 
 const app = express();
-let corsOptions = { 
-  origin : ['https://www.supersiesta-home.tn'], 
-} 
- 
-app.use(cors(corsOptions))
+const PORT = process.env.PORT || 4000;
+
+// CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://www.supersiesta-home.tn');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+// Other middleware
 app.use(express.json());
 
+// Routes
 app.use("/api/register", registerRoute);
 app.use("/api/login", loginRoute);
 app.use("/api/users", usersRoute);
@@ -26,12 +32,10 @@ app.use("/api/products", productsRoute);
 app.use("/api/clients", clientsRoute);
 app.use("/api/orders", ordersRoute);
 
-const PORT = process.env.PORT || 4000;
-
+// Start the server
 const startServer = async () => {
   try {
-    connectDB(process.env.MONGODB_URI);
-
+    await connectDB(process.env.MONGODB_URI);
     app.listen(PORT, () => {
       console.log(`Server is listening on port ${PORT}`);
     });
